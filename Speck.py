53a9880862a3abcdc0d23aefffecc3fb840b5a6d
rpoli <rpoliquin01@gmail.com>
2025-02-07 11:47:47 -0500

    Finished the first draft layout of the base code.

--- Speck.py
+++ Speck.py
@@ -15,9 +15,13 @@ ________________________________________________________________________________
 References:
     Python. (2025, January 8). Python 3.13.1 documentation. Retrieved from Python: https://docs.python.org/3/
     Geeks for Geeks. (2024, August 2). Python Docstrings. Retrieved from Geeks for Geeks: https://www.geeksforgeeks.org/python-docstrings/
+    https://forums.raspberrypi.com/viewtopic.php?t=173157
+    https://stackoverflow.com/questions/8247605/configuring-so-that-pip-install-can-work-from-github 
 """
 # __________Import Statements__________
 import numpy as np
+from gpiozero import Servo
+import subprocess
 
 # __________Global Variables__________
 # Create an array of boolean values to keep track of what GPIO pins are available on the pi
@@ -58,7 +62,7 @@ class Joint:
 
     def set_angle(self, angle:int):
         """
-        A function used to set the angle of the joint
+        A function used to set the angle of the joint.
 
         :param angle: int: the angle to set the joint to
         :return: None
@@ -67,13 +71,13 @@ class Joint:
         if angle <= self.max_angle & angle >= self.min_angle:
             self.current_angle = angle  # update the current angle of the joint to the required angle
         else:
-            raise RuntimeError("The given angle was out of the joint's range " + str(self.min_angle) + )
+            raise RuntimeError("The given angle was out of the joint's range " + str(self.min_angle))
             pass
         return None
 
     def change_angle(self, change_in_angle:int):
         """
-        A function used to change the angle of the joint
+        A function used to change the angle of the joint.
 
         :param change_in_angle: int: the amount to change the angle of the joint
         :return: None
@@ -96,13 +100,29 @@ class Leg:
         """
         Constructor for the Leg class.
         """
-        self.hip_lat = Joint(hip_lat_pin, starting_angle=0)
-        self.hip_long = Joint(hip_long_pin, starting_angle=0)
-        self.knee = Joint(knee_pin, starting_angle=0)
+        # check to make sure all given pins are available. Raise an error if the pin is unavailable. Set the pins to taken
+        if AvailablePins[hip_lat_pin]:
+            self.hip_lat = Joint(hip_lat_pin, starting_angle=0)
+            AvailablePins[hip_lat_pin] = False
+        else:
+            raise RuntimeError("Pin " + str(hip_lat_pin) + " is not available to use for the lateral hip joint.")
+
+        if AvailablePins[hip_long_pin]:
+            self.hip_long = Joint(hip_long_pin, starting_angle=0)
+            AvailablePins[hip_long_pin] = False
+        else:
+            raise RuntimeError("Pin " + str(hip_long_pin) + " is not available to use for the longitudinal hip joint.")
+
+        if AvailablePins[knee_pin]:
+            self.knee = Joint(knee_pin, starting_angle=0)
+            AvailablePins[knee_pin] = False
+        else:
+            raise RuntimeError("Pin " + str(knee_pin) + " is not available for the knee joint")
+        self.current_position = {0,0,0}
 
     def set_position(self, x, y, z):
         """
-        A function used to set the position of the foot. The position is relative to ___
+        A function used to set the position of the foot. The position is relative to ___.
 
         :param x:
         :param y:
@@ -110,6 +130,18 @@ class Leg:
         :return: None
         """
         return None
+
+    def get_position(self):
+        """
+        A function used to get the current position of the foot of the leg based on the joint angles.
+
+        :return: an array containing the current location of the foot in the form {x, y, z}
+        """
+        # :TODO perform trig calculations to determine leg position based on joint angles
+        x = 0
+        y = 0
+        z = 0
+        return {x, y, z}
 
     def move(self, x, y, z):
         """
@@ -127,7 +159,9 @@ class Leg:
 
 class ObjectDetector:
     """
-    The ObjectDectecor class is used to represent a single infrared avoidance sensor
+    The ObjectDectecor class is used to represent a single infrared avoidance sensor.
+
+    :param pin: int: the GPIO pin that the sensor is connected to
     """
 
     def __init__(self, pin:int):
@@ -139,15 +173,15 @@ class Speck:
     """
     The Speck class is used to control the primary functions of Speck including motion, object detection, and crate grabbing.
 
-    :param self.rf_leg: a Leg object representing the right front leg of Speck
-    :param self.lf_leg: a Leg object representing the left front leg of Speck
-    :param self.rb_leg: a Leg object representing the right back leg of Speck
-    :param self.lb_leg: a Leg object representing the left back leg of Speck
+    :param self.rf_leg: Leg: a Leg object representing the right front leg of Speck
+    :param self.lf_leg: Leg:a Leg object representing the left front leg of Speck
+    :param self.rb_leg: Leg: a Leg object representing the right back leg of Speck
+    :param self.lb_leg: Leg: a Leg object representing the left back leg of Speck
     """
 
     def __init__(self):
         """
-        Constructor for the Speck Class.
+        Constructor for the Speck Class. Used to initialize Speck
         """
         self.rf_leg = Leg(1,2,3)
         self.lf_leg = Leg(4,5,6)
@@ -158,7 +192,34 @@ class Speck:
     def step(self):
         pass
 
+    def stand(self):
+        """
+        Function used to set Speck
+        """
+        self.lf_leg.set_position()
+        self.rf_leg.set_position()
+        self.lb_leg.set_position()
+        self.rb_leg.set_position()
+
+    def sit(self):
+        self.lf_leg.set_position()
+        self.rf_leg.set_position()
+        self.lb_leg.set_position()
+        self.rb_leg.set_position()
+
     def update(self):
         successful = False
-
+        wifi_ip = subprocess.check_output(['hostname', '-I'])
+        if wifi_ip is not None: # Wifi is connected, so Speck can be updated
+            subprocess.run(['sudo', 'apt', 'update']) # update the package list
+            subprocess.run(['sudo', 'apt', 'upgrade']) # update the packages
+            subprocess.run(['pip', 'install', '--upgrade', 'pip']) # update pip
+            subprocess.run(['pip', 'install', '--upgrade', 'gpiozero']) # update gpiozero
+            subprocess.run(['pip', 'install', '--upgrade', 'numpy']) # update numpy
+            subprocess.run(['git', 'clone', 'git@github.com:rpoliqui/Speck.git']) # update Speck code: https://github.com/rpoliqui/Speck
+            subprocess.run(['cd', 'Speck']) # change directory to Speck
+            subprocess.run(['pip', 'install', '-e', '.']) # install the Speck code
+            successful = True # Speck was successfully updated
+        else: # wifi is not connected, Speck cannot be updated
+            print("Speck cannot be updated without a wifi connection.") 
         return successful
