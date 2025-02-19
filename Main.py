45ee094510c18c410ca515905b4a466ab79d27e7
rpoli <rpoliquin01@gmail.com>
2025-02-18 23:28:37 -0500

    updated Main.py to include a setup function and a loop function to simplify the main loop code.

--- Main.py
+++ Main.py
@@ -1,4 +1,18 @@
-import Speck
+from Speck import Speck
+
+
+def setup(speck:Speck):
+    speck.lb_leg.set_position(0, 0, 0)
+    return None
+
+
+def loop():
+    cont = False
+    return cont
+
 
 if __name__ == "__main__":
-    speck = Speck()
+    speck = Speck()  # create an instance of the Speck object to control
+    setup(speck)  # perform all necessary setup tasks
+    while loop():  # continue performing all functions in the loop function while the output is true
+        pass
