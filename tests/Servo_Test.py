cd910f05b2201afbd80860cf67754961e29c90ba
rpoli <rpoliquin01@gmail.com>
2025-02-21 09:37:14 -0500

    Created update_Speck.py to quickly pull code from repo. Also create Servo_Test.py to troubleshoot servo problems.

--- tests/Servo_Test.py
+++ tests/Servo_Test.py
@@ -1,7 +1,10 @@
 from gpiozero import AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
 import time
 
-test_servo = AngularServo(14, min_pulse_width=0.0006, max_pulse_width=0.0025)
+factory = PiGPIOFactory()  # define pin factory to use servos for more accurate servo control
+
+test_servo = AngularServo(14, min_pulse_width=0.0006, max_pulse_width=0.0023)
 while True:
     test_servo.min()
     time.sleep(5)
