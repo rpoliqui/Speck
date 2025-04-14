af3f61d36b7459ea969c4605570ba1c72d19fa05
rpoli <rpoliquin01@gmail.com>
2025-04-14 09:16:07 -0400

    Make Speck stand when initialized.

--- Main.py
+++ Main.py
@@ -2,5 +2,6 @@ import subprocess
 from Speck import Speck
 
 if __name__ == "__main__":
-    subprocess.run(['python3', '/home/speck/Speck/updateSpeck.py']) # autoupdate Speck
+    subprocess.run(['python3', '/home/speck/Speck/updateSpeck.py'])  # auto update Speck
     speck = Speck()  # create an instance of the Speck object to control
+    speck.stand()
