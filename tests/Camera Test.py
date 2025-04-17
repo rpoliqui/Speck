3eccc978d1660362d4e8d56eda2e717b39001034
rpoli <rpoliquin01@gmail.com>
2025-04-16 23:26:24 -0400

    Updated to use Picamera2 package

--- tests/Camera Test.py
+++ tests/Camera Test.py
@@ -1,15 +1,11 @@
-from picamera import PiCamera
+from picamera2 import Picamera2
 import time
 
-camera = PiCamera()
+camera = Picamera2()
+camera.start()
 
-camera.resolution = (1280, 720)
-camera.vflip = True
-
-camera.start_preview()
 time.sleep(2)
 
-camera.capture('/home/speck/Speck/tests/image.jpg')
+camera.capture_file('test.jpg')
 
-camera.stop_preview()
 camera.close()
