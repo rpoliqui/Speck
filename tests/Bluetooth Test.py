ee5e06bbac46a38a4ad6e5b9eb9dc87c3bbabb9b
rpoli <rpoliquin01@gmail.com>
2025-04-18 10:17:36 -0400

    updated bluetooth testing code.

--- tests/Bluetooth Test.py
+++ tests/Bluetooth Test.py
@@ -2,6 +2,28 @@ import bluetooth
 import subprocess
 from threading import Thread
 
+import subprocess
+import time
+import os
+
+
+def setup_bluetoothctl():
+    print("\n[Setting up Bluetooth...]")
+    bluetoothctl_commands = f"""
+                            power on
+                            agent on
+                            discoverable on
+                            pairable on
+                            scan on
+                            """
+
+    # Run bluetoothctl with input commands
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    out, err = process.communicate(bluetoothctl_commands)
+    print(out)
+    if err:
+        print("[Bluetoothctl Error]", err)
+
 
 def bluetooth_server():
     server_sock.listen(1)
@@ -29,10 +51,10 @@ def bluetooth_server():
 
 if __name__ == '__main__':
     subprocess.run(['service', 'bluetooth', 'start'])  # start bluetooth on pi
-
+    setup_bluetoothctl()
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     port = 3
     server_sock.bind(("", port))
 
-    bluetooth_thread = Thread(target=bluetooth_server, daemon=False)
+    bluetooth_thread = Thread(target=bluetooth_server, daemon=True)
     bluetooth_thread.start()
