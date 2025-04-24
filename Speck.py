86eaadb53c188e61f73ebbd3427594b7ce971381
rpoli <rpoliquin01@gmail.com>
2025-04-24 11:32:24 -0400

    Updated walking code

--- Speck.py
+++ Speck.py
@@ -142,8 +142,11 @@ WALK_GAIT = (([2], 0, -50, 0),
                ([4], 25, 0, 0),
 
                ([3], 0, -50, 0),
+               ([0, 1, 3], 0, 0, 0),
                ([3], -50, 0, 0),
+               ([0, 1, 3], 0, 0, 0),
                ([3], 0,  50, 0),
+               ([0, 1, 3], 0, 0, 0),
 
                ([4], 25, 0, 0))
 
@@ -1081,10 +1084,10 @@ class Speck:
         """
         self.is_standing = False
         for i in range(0, 2, 1):
-            self.move_queues[i].put([4, 25 - self.Legs[i].current_position[0], 175 - self.Legs[i].current_position[1],
+            self.move_queues[i].put([4, 0 - self.Legs[i].current_position[0], 175 - self.Legs[i].current_position[1],
                                      HIP_LENGTH - self.Legs[i].current_position[2]])
         for i in range(2, 4, 1):
-            self.move_queues[i].put([4, 25 - self.Legs[i].current_position[0], 175 - self.Legs[i].current_position[1],
+            self.move_queues[i].put([4, 50 - self.Legs[i].current_position[0], 175 - self.Legs[i].current_position[1],
                                      HIP_LENGTH - self.Legs[i].current_position[2]])
 
     def set_sit(self):
